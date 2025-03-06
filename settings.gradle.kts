0984d4b96fd769fd4228999ff487e2fb91060fb5
Neo <c.min.tseng@gmail.com>
2025-03-06 23:49:39 +0800

    modify repos

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,15 +1,33 @@
 pluginManagement {
     repositories {
-        google()
-        mavenCentral()
         gradlePluginPortal()
+        maven(url = "https://plugins.gradle.org/m2/")
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        maven(url = "https://jitpack.io")
+        maven(url = "https://s3.amazonaws.com/repo.commonsware.com")
+        maven(url = "https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
+
+@Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven(url = "https://jitpack.io")
+        maven(url = "https://s3.amazonaws.com/repo.commonsware.com")
+        maven(url = "https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        maven(url = "https://www.dfki.uni-kl.de/artifactory/libs-releases/")
+        maven(url = "https://jogamp.org/deployment/maven")
+        maven(url = "https://raw.githubusercontent.com/cybernhl/maven-repository/master/")
+        maven(url = "https://api.xposed.info/")
     }
 }
 
